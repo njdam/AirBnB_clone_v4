e2b85406096e02732863365c32d2fed1e43c5df6
njdam <jeandamngaboyimana@gmail.com>
2023-11-04 17:50:42 +0000

    2. Select some Amenities to be comfortable!

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+// Use strict mode to avoid coding errors
+'use strict';
+// A javascript to listen for changes on each input checkbox tag.
+$(document).ready(init);
+
+function init () {
+  const amenityObject = {};
+  $('.amenities .popover input').change(function () {
+    if ($(this).is(':checked')) {
+      amenityObject[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else if ($(this).is(':not(:checked)')) {
+      delete amenityObject[$(this).attr('data-name')];
+    }
+    const names = Object.keys(amenityObject);
+    $('.amenities h4').text(names.sort().join(', '));
+  });
+}
